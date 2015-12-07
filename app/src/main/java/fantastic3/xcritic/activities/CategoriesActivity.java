61bb5844ce81c57a537e1fac29fac10e43d3710c
Juan Pablo Podlech Urrutia <juanpablopodlech@gmail.com>
2015-12-07 01:28:13 -0500

    Add favorites menu.

--- app/src/main/java/fantastic3/xcritic/activities/CategoriesActivity.java
+++ app/src/main/java/fantastic3/xcritic/activities/CategoriesActivity.java
@@ -3,6 +3,8 @@ package fantastic3.xcritic.activities;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import fantastic3.xcritic.R;
 import fantastic3.xcritic.fragments.CategoriesFragment;
@@ -30,5 +32,15 @@ public class CategoriesActivity extends AppCompatActivity {
     private void setupCategoriesSelector(FragmentTransaction ft) {
         CategoriesFragment cf = CategoriesFragment.newInstance(null);
         ft.replace(R.id.flCategories, cf);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.categories, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public void onFavorites(MenuItem item) {
     }
 }
